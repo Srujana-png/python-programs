ca85757626d0cab3140e80d86a5ef1b1f4b9e09b
Srujana-png <vijayaramanathi@gmail.com>
2026-04-27 06:23:53 +0530

    Add files via upload

---
+++ function arguments.py
@@ -0,0 +1,10 @@
+# positional arg
+def greet(name="hello",age=24):
+    print(f'my name is {name} and i am {age}years old')
+greet("hello",24)
+greet(24,"hello")
+
+
+#lambda functions
+c=lambda a,b:a+b
+  print(c)
